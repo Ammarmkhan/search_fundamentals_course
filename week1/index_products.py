9cd0bd9dd8aa61ef59a9048fbf16b2acbdf18f84
Ammar Khan <81581555+Ammarmkhan@users.noreply.github.com>
2022-06-22 21:26:35 -0400

    Establishing connection with OpenSearch

--- week1/index_products.py
+++ week1/index_products.py
@@ -8,6 +8,7 @@ from opensearchpy import OpenSearch
 from opensearchpy.helpers import bulk
 import logging
 import time
+import json
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -60,9 +61,19 @@ def get_opensearch():
     auth = ('admin', 'admin')
 
     #### Step 2.a: Create a connection to OpenSearch
-    client = None
+    client = OpenSearch(
+    hosts = [{'host': host, 'port': port}],
+    http_compress = True, # enables gzip compression for request bodies
+    http_auth = auth,
+    \# client_cert = client_cert_path,
+    \# client_key = client_key_path,
+    use_ssl = True,
+    verify_certs = False,
+    ssl_assert_hostname = False,
+    ssl_show_warn = False,
+    )
+    
     return client
-
 
 @click.command()
 @click.option('--source_dir', '-s', help='XML files source directory')
